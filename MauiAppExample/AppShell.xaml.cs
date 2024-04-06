237a61b7e024e50181ee9d1ed2088a8ae201da8b
developertest_02 <developertest02@gmail.com>
2024-04-06 08:54:43 -0400

    building

--- MauiAppExample/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MauiAppExample;
-
-public partial class AppShell : Shell
-{
-    public AppShell()
-    {
-        InitializeComponent();
-    }
-}
--- MauiExample/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MauiExample;
-
-public partial class AppShell : Shell
-{
-    public AppShell()
-    {
-        InitializeComponent();
-    }
-}
